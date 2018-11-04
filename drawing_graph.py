42186d356d97a5cd596e1c55620e1ab7d46ea51c
Scherbitskaya Irina <31989910+irina-scherbitskaya@users.noreply.github.com>
2018-11-05 00:19:21 +0300

    Update drawing_graph.py

--- drawing_graph.py
+++ drawing_graph.py
@@ -5,7 +5,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 fig = plt.gcf()
-fig.set_size_inches(20.5, 20)
+fig.set_size_inches(20, 20)
     
 def drawing_graph(file):
     fig.clear()
@@ -15,12 +15,14 @@ def drawing_graph(file):
     G.add_weighted_edges_from([(e['points'][0], e['points'][1], e['length']) for e in data['lines']])
 
     labels = nx.get_edge_attributes(G,'weight')
-    pos = nx.spectral_layout(G)
+    pos = nx.kamada_kawai_layout(G)
     nx.draw_networkx_edge_labels(G, pos, font_size = 8, edge_labels = labels)
     nx.draw_networkx_labels(G, pos, font_size = 8)
     nx.draw(G,pos, node_color = '#DDA0DD')
 
     fig.savefig('%s.png' % file)
 
-drawing_graph('small_graph')
-drawing_graph('big_graph')
+list_graphs = ['small_graph', 'big_graph', 'first', 'second', 'third']
+
+for graph in list_graphs:
+    drawing_graph(graph)
