7c8459d8d99103db73112cb4e185f26878258bdc
Scherbitskaya Irina <31989910+irina-scherbitskaya@users.noreply.github.com>
2018-11-05 00:55:19 +0300

    Update drawing_graph.py

--- drawing_graph.py
+++ drawing_graph.py
@@ -13,11 +13,10 @@ def drawing_graph(file):
     G = nx.Graph()
     G.add_nodes_from([v['idx'] for v in data['points']])
     G.add_weighted_edges_from([(e['points'][0], e['points'][1], e['length']) for e in data['lines']])
-    labels = nx.get_edge_attributes(G,'weight')
+    labels = nx.get_edge_attributes(G, 'weight')
     pos = nx.kamada_kawai_layout(G, weight = None)
     nx.draw_networkx_edge_labels (G, pos, font_size = 10, edge_labels = labels)
     nx.draw_networkx(G, pos = pos, node_color = '#DDA0DD', with_labels = True, font_size = 10, node_size = 400)
-
     fig.savefig('%s.png' % file)
 
 list_graphs = ['small_graph', 'big_graph', 'first', 'second', 'third']
